e0e2745268cb4198e166821ace2e49fb00de77c9
PixelTijgers <57810027+PixelTijgers@users.noreply.github.com>
2022-02-01 13:20:25 +0100

    Minor feedback changes.

--- resources/views/fr/includes/request.blade.php
+++ resources/views/fr/includes/request.blade.php
@@ -1,5 +1,7 @@
 <div class="flex flex-col justify-center md:pt-5 md:pb-5 lg:pt-10 lg:pb-10" id="request">
 
+    <h3 class="text-4xl mt-10 mb-5">Request</h3>
+
     <div class="embed-container">
 
         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSclq7hZBMVxloOEgPIFtpFl_jVkg5Z1hT4nQdy6BJEWovKOig/viewform?embedded=true" width="640" height="1428" frameborder="0" marginheight="0" marginwidth="0">Laden…</iframe>
